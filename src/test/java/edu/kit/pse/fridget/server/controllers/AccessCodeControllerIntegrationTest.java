4a5174c1947e5ad1d571e1c35751ac4ade91ebcd
YuKitAs <moncher.merci@gmail.com>
2018-07-02 23:44:17 +0200

    Integration test for AccessCodeController

---
+++ src/test/java/edu/kit/pse/fridget/server/controllers/AccessCodeControllerIntegrationTest.java
@@ -0,0 +1,25 @@
+package edu.kit.pse.fridget.server.controllers;
+
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import edu.kit.pse.fridget.server.models.AccessCode;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AccessCodeControllerIntegrationTest extends AbstractControllerIntegrationTest {
+
+    @Test
+    public void generateAccessCode() throws Exception {
+        AccessCode accessCode = getFixture("accessCode.json", AccessCode.class);
+        ResponseEntity<AccessCode> response = getTestRestTemplate().postForEntity("/access-codes", accessCode, AccessCode.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(response.getBody()).satisfies(body -> {
+            assertThat(body.getId()).matches(UUID_PATTERN);
+            assertThat(body.getFlatshareId()).isEqualTo("00000000-0000-0000-0000-000000000000");
+            assertThat(body.getContent()).matches("[0-9a-zA-Z]{5}");
+        });
+    }
+}
