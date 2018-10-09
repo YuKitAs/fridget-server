3ed80fcb3fdf1faa02c82bee7ffc4aca6da691d9
YuKitAs <moncher.merci@gmail.com>
2018-10-10 01:04:50 +0200

    Update boot-run.sh

--- boot-run.sh
+++ boot-run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 SPRING_PROFILES_ACTIVE=development ./gradlew clean bootRun
+
+# For Gradle 4.9 or later, use the following command instead.
+# ./gradlew bootRun --args='--spring.profiles.active=development'
